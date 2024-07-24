12565e063e14206bfbc37b24faa78ac9bb9b53e9
YOmprakash <omprakashyamavaram@gmail.com>
2024-07-24 21:19:52 +0530

    footer section completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { SOCIAL_MEDIA_LINKS } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="text-white">Footer</div>
-  )
-}
+    <section className="text-white mb-8 mt-6">
+      <div className="flex items-center justify-center gap-8 ">
+        {SOCIAL_MEDIA_LINKS.map((link) => (
+          <a href={link.href} key={link.key} className={link.className}>
+            {link.icon}
+          </a>
+        ))}
+      </div>
+      <p className="text-center mt-4 tracking-tighter text-neutral-500">
+        &copy;compileTab. All rights reserved.
+      </p>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
