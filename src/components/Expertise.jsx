c3efcc528b548e2db3b4119b233e6c3817b20b71
YOmprakash <omprakashyamavaram@gmail.com>
2024-07-25 10:31:21 +0530

    final commit

--- src/components/Expertise.jsx
+++ src/components/Expertise.jsx
@@ -1,28 +1,68 @@
-
-import {CUSINES} from "../constants";
+import { CUSINES } from "../constants";
 import { motion } from "framer-motion";
+
+const containerVarients = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
 const Expertise = () => {
   return (
     <section className="mb-8">
-    <h2 className="my-4 text-white text-center text-3xl lg:text-4xl">Our Expertise</h2>
-    <div className="container mx-auto px-4">
-    {CUSINES.map((cuisine, index) => (
-      <div key={index} className="flex items-center border-dotted border-b-4 border-neutral-700/40 py-2">
-      <div className="pr-8">{cuisine.number}</div>
-      <div className="w-1/3 flex-shrink-0">
-        <img src={cuisine.image} alt={cuisine.title} className="h-auto rounded-3xl"/>
-      </div>
-      <div className="pl-4">
-        <h3 className="text-2xl uppercase text-rose-300">{cuisine.title}</h3>
-        <p className="text-lg mt-4">{cuisine.description}</p>
-      </div>
-
-      </div>
-       
-    ))}
-    </div>
+      <h2 className="my-4 text-white text-center text-3xl lg:text-4xl">
+        Our Expertise
+      </h2>
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        variants={containerVarients}
+        className="container mx-auto px-4"
+      >
+        {CUSINES.map((cuisine, index) => (
+          <motion.div
+            variants={itemVariants}
+            key={index}
+            className="flex items-center border-dotted border-b-4 border-neutral-700/40 py-2"
+          >
+            <div className="pr-8">{cuisine.number}</div>
+            <div className="w-1/3 flex-shrink-0">
+              <img
+                src={cuisine.image}
+                alt={cuisine.title}
+                className="h-auto rounded-3xl"
+              />
+            </div>
+            <div className="pl-4">
+              <h3 className="text-2xl uppercase text-rose-300">
+                {cuisine.title}
+              </h3>
+              <p className="text-lg mt-4">{cuisine.description}</p>
+            </div>
+          </motion.div>
+        ))}
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Expertise
+export default Expertise;
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -5,26 +5,72 @@ import customer1 from "../assets/customer1.jpeg"
 import customer2 from "../assets/customer2.jpeg"
 import customer3 from "../assets/customer3.jpeg"
 import customer4 from "../assets/customer4.jpeg"
+import { motion } from "framer-motion";
 
+const containerVarients = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.8,
+      ease: "easeInOut",
+    },
+  },
+}
+
+const itemVarients = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+}
 const Review = () => {
   return (
     <section className="container mx-auto mb-8 mt-12  ">
-    <div className="flex flex-col justify-center items-center">
-      <p className="text-3xl px-2 font-light leading-normal lg:mx-40 lg:text-[52px]">{REVIEW.content}</p>
-      <div className=" mt-10 flex  justify-center items-center">
+    <motion.div
+     initial="hidden"
+     whileInView="show"
+     viewport={{ once: true }}
+     variants={containerVarients}
+
+     className="flex flex-col justify-center items-center">
+      <motion.p
+      variants={itemVarients}
+
+       className="text-3xl px-2 font-light leading-normal lg:mx-40 lg:text-[52px]">{REVIEW.content}</motion.p>
+      <motion.div
+      variants={itemVarients}
+       className=" mt-10 flex  justify-center items-center">
         <img className="rounded-full w-12 f" src={xaviour} alt="xaviour" width={80} height={80} />
         <div className="ml-4">
           <h6>{REVIEW.name}</h6>
           <p className="text-sm text-neutral-500">{REVIEW.profession}</p>
         </div>
-      </div>
+      </motion.div>
 
-    </div>
-    <div className="flex flex-col gap-2 items-center justify-center flex-wrap mt-8 md:flex-row">
+    </motion.div>
+    <motion.div
+     initial="hidden"
+     whileInView="show"
+     viewport={{ once: true }}
+     variants={itemVarients}
+     className="flex flex-col gap-2 items-center justify-center flex-wrap mt-8 md:flex-row">
       {[customer1, customer2, customer3, customer4].map((image, index) => (
-        <img className='rounded-lg object-cover w-[200px] h-[300px] rounded-br-3xl rounded-tl-3xl' key={index} src={image} alt="xaviour"  />
+        <motion.img 
+        variants={itemVarients}
+        className='rounded-lg object-cover w-[200px] h-[300px] rounded-br-3xl rounded-tl-3xl' key={index} src={image} alt="xaviour"  />
       ))}
-    </div>
+    </motion.div>
 
     </section>
   )
